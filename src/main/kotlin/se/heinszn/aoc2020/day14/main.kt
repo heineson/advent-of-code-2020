d7a4b953595bf7166b20a3cbcd4fc0a7ea7c07ae
Jonas Heineson <heineson@gmail.com>
2020-12-14 09:46:04 +0100

    Day 14/2 WIP

--- src/main/kotlin/se/heinszn/aoc2020/day14/main.kt
+++ src/main/kotlin/se/heinszn/aoc2020/day14/main.kt
@@ -20,9 +20,39 @@ fun programStep(step: Triple<String, Long, String>, state: State): State {
 fun updateMemory(value: Long, state: State): Long {
     val bits = value.toString(2).padStart(36, '0')
     val result = bits.zip(state.currMask) { b, m -> if (m == 'X') b else m }
-    println("$bits + ${state.currMask} = ${String(result.toCharArray())}")
+//    println("$bits + ${state.currMask} = ${String(result.toCharArray())}")
     return java.lang.Long.parseLong(String(result.toCharArray()), 2)
 }
+
+
+fun programStep2(step: Triple<String, Long, String>, state: State): State {
+    return when(step.first) {
+        "mask" -> state.copy(currMask = step.third)
+        "mem" -> {
+            updateMemory2(step.second, step.third.toLong(), state)
+            return state
+        }
+        else -> throw IllegalStateException("No such op")
+    }
+}
+
+fun updateMemory2(pos: Long, value: Long, state: State){
+    val bits = pos.toString(2).padStart(36, '0')
+    val result = bits.zip(state.currMask) { b, m -> if (m == '0') b else m }
+
+    val list = loop(String(result.toCharArray()), mutableListOf())
+    println(list.joinToString(", "))
+
+    for (addr in list) {
+        state.currMem[java.lang.Long.parseLong(String(addr.toCharArray()), 2)] = value
+    }
+}
+
+fun loop(mask: String, acc: MutableList<String>): List<String> {
+    val xs = mask.mapIndexed { i, c -> Pair(i, c) }.filter { p -> p.second == 'X' }
+    return if (xs.isEmpty()) acc else acc + loop(mask.replaceFirst('X', '1'), acc) + loop(mask.replaceFirst('X', '0'), acc)
+}
+
 
 fun lineParser(l: String): Triple<String, Long, String> {
     val parts = l.split(" = ")
@@ -31,15 +61,28 @@ fun lineParser(l: String): Triple<String, Long, String> {
     return Triple(op, pos, parts[1])
 }
 
-fun main() {
-//    val data = testData.map { lineParser(it) }
+fun part1() {
+    //    val data = testData.map { lineParser(it) }
     val data = readFile({ lineParser(it) })
 
     var state = State("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", mutableMapOf())
     data.forEach { state = programStep(it, state) }
 
     println("Part1: ${state.currMem.values.sum()}")
+}
 
+fun part2() {
+    val data = testData2.map { lineParser(it) }
+
+    var state = State("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", mutableMapOf())
+    data.forEach { state = programStep2(it, state) }
+
+    println("Part2: ${state.currMem.values.sum()}")
+}
+
+fun main() {
+    part1()
+    part2()
 }
 
 val testData = """
@@ -47,4 +90,11 @@ val testData = """
     mem[8] = 11
     mem[7] = 101
     mem[8] = 0
+""".trimIndent().split("\n")
+
+val testData2 = """
+    mask = 000000000000000000000000000000X1001X
+    mem[42] = 100
+    mask = 00000000000000000000000000000000X0XX
+    mem[26] = 1
 """.trimIndent().split("\n")
