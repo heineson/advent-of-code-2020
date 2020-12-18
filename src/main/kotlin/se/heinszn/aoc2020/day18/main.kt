7bbe92c1834743839e561d19c31ba4ddae44a33e
Jonas Heineson <jonas.heineson@seal-software.com>
2020-12-18 10:53:36 +0100

    Day 18/1 - all examples

--- src/main/kotlin/se/heinszn/aoc2020/day18/main.kt
+++ src/main/kotlin/se/heinszn/aoc2020/day18/main.kt
@@ -63,15 +63,14 @@ fun parseTokens(ts: List<String>, right: Expr?): Expr {
                     }
                 }
 
-                //val itemCount = rest.takeLastWhile { !it.startsWith("(") }.count() + 1
                 val its = rest.takeLast(itemCount)
-                println("Test: $itemCount $its")
+//                println("Test: $itemCount $its")
                 val items = listOf(its.first().removePrefix("(")) + its.drop(1) + el
 
 
                 val l = rest.dropLast(itemCount + 1)
                 val op = rest.dropLast(itemCount).lastOrNull()
-                println("Expr(parseTokens($l), parseTokens(${items})), $op")
+//                println("Expr(parseTokens($l, $right), parseTokens($items, $right)), $op")
                 return Expr(parseTokens(l, right), parseTokens(items, right), if (op == null) null else parseOp(op) )
             }
             throw IllegalArgumentException("Should not get here, value: $elem")
@@ -86,16 +85,19 @@ fun eval(e: Expr): Long {
     if (e.op != null) {
         return when (e.op) {
             Op.PLUS -> {
-                println("Eval: ${e.l} + ${e.r}")
+//                println("Eval: ${e.l} + ${e.r}")
                 eval(e.l!!) + eval(e.r!!)
             }
             Op.TIMES -> {
-                println("Eval: ${e.l} * ${e.r}")
+//                println("Eval: ${e.l} * ${e.r}")
                 eval(e.l!!) * eval(e.r!!)
             }
         }
     }
-    throw IllegalStateException("No op and no number: ${e.l}, ${e.r}")
+    if (e.r != null) {
+        return eval(e.r)
+    }
+    throw IllegalStateException("No op and no number and no right: ${e.l}, ${e.r}")
 }
 
 fun main() {
