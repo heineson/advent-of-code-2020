a4fefa3b302418bd105042e242723c8e1783776c
Jonas Heineson <jonas.heineson@seal-software.com>
2020-12-18 12:41:36 +0100

    Day 18/2

--- src/main/kotlin/se/heinszn/aoc2020/day18/main.kt
+++ src/main/kotlin/se/heinszn/aoc2020/day18/main.kt
@@ -69,7 +69,6 @@ fun parseTokens(ts: List<String>, right: Expr?): Expr? {
 //                println("Test: $itemCount $its")
                 val items = listOf(its.first().removePrefix("(")) + its.drop(1) + el
 
-
                 val l = rest.dropLast(itemCount + 1)
                 val op = rest.dropLast(itemCount).lastOrNull()
 //                println("Expr(parseTokens($l, $right), parseTokens($items, $right)), $op")
@@ -102,6 +101,24 @@ fun eval(e: Expr): Long {
     throw IllegalStateException("No op and no number and no right: ${e.l}, ${e.r}")
 }
 
+val addGroupRegex = """(?:[^(]|\d+) \+ (\d+|\(.*\))""".toRegex()
+fun prepare(e: String): String {
+    print("Preparing: $e -> ")
+    var res = addGroupRegex.replace(e) { m -> "(${m.value})"}
+//    var res = e
+//    val matches = addGroupRegex.find(e)
+//    if (matches != null) {
+//        var m = matches.next()
+//        while (m != null) {
+//
+//            m = matches.next()
+//        }
+//    }
+    res = res.replace(")()", "))")
+    println(res)
+    return res
+}
+
 fun main() {
 //    val data = testData.map { parser(it) }
     val data = readFile({ parser(it) })
@@ -110,10 +127,52 @@ fun main() {
     data.forEach { println(eval(it)) }
     println()
     println("Part 1: ${data.map { eval(it) }.sum()}")
+    println()
+
+//    val data2 = testData.map { parser(prepare(it)) }
+//    val data2 = readFile({ parser(prepare(it)) })
+//    data2.forEach { println("$it: ${eval(it)}") }
+//    println()
+//    println("Part 2: ${data2.map { eval(it) }.sum()}")
+
+//    println("Part 2: ${testData.map { testIdea(it) }.sum()}")
+    println("Part 2: ${readFile({ solvePart2(it) }).sum()}")
 }
+
+val groupR = """\(([\d +*]+)\)""".toRegex()
+val addR = """(\d+) \+ (\d+)""".toRegex()
+val mulR = """(\d+) \* (\d+)""".toRegex()
+fun solvePart2(expr: String): Long {
+    print("Evaluating: $expr: ")
+    var tmp = "($expr)"
+    while (tmp.contains('(')) {
+        tmp = groupR.replace(tmp) { parseGroup(it.value) }
+    }
+    println(tmp)
+    return tmp.toLong()
+}
+
+fun parseGroup(g: String): String {
+    var t = g.removePrefix("(").removeSuffix(")")
+    while (t.contains('+')) {
+        t = addR.replace(t) {
+            val (a, b) = it.destructured
+            "${a.toLong() + b.toLong()}"
+        }
+    }
+    while (t.contains('*')) {
+        t = mulR.replace(t) {
+            val (a, b) = it.destructured
+            "${a.toLong() * b.toLong()}"
+        }
+    }
+    return t
+}
+
 
 val testData = """
     1 + 2 * 3 + 4 * 5 + 6
+    1 + (2 * 3) + (4 * (5 + 6))
     2 * 3 + (4 * 5)
     5 + (8 * 3 + 9 + 3 * 4 * 3)
     5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
