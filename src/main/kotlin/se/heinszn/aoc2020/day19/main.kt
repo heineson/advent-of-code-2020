6ac88861a2dac63e7a3df2931a54f0d7b19706df
Jonas Heineson <heineson@gmail.com>
2020-12-19 16:46:51 +0100

    Day 19 setup

---
+++ src/main/kotlin/se/heinszn/aoc2020/day19/main.kt
@@ -0,0 +1,22 @@
+package se.heinszn.aoc2020.day19
+
+fun main() {
+
+}
+
+val testRules = """
+    0: 4 1 5
+    1: 2 3 | 3 2
+    2: 4 4 | 5 5
+    3: 4 5 | 5 4
+    4: "a"
+    5: "b"
+""".trimIndent().split("\n")
+
+val testMsgs = """
+    ababbb
+    bababa
+    abbbab
+    aaabbb
+    aaaabbb
+""".trimIndent().split("\n")
