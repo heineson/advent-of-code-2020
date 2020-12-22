f06d78254cef5bbbbfefdb4f6a48f26704834ee0
Jonas Heineson <heineson@gmail.com>
2020-12-22 12:04:41 +0100

    Day 22/2

--- src/main/kotlin/se/heinszn/aoc2020/day22/main.kt
+++ src/main/kotlin/se/heinszn/aoc2020/day22/main.kt
@@ -1,6 +1,15 @@
 package se.heinszn.aoc2020.day22
 
 typealias Deck = List<Int>
+
+fun game(startingDecks: Pair<Deck, Deck>): Pair<Deck, Deck> {
+    var decks = startingDecks
+    while (decks.first.isNotEmpty() && decks.second.isNotEmpty()) {
+        decks = round(decks)
+    }
+    return decks
+}
+
 fun round(decks: Pair<Deck, Deck>): Pair<Deck, Deck> {
     val (p1, p2) = decks
     return if (p1.first() > p2.first()) {
@@ -10,21 +19,67 @@ fun round(decks: Pair<Deck, Deck>): Pair<Deck, Deck> {
     }
 }
 
+var gameCnt = 0
+
+fun recursiveGame(startingDecks: Pair<Deck, Deck>): Pair<Deck, Deck> {
+    gameCnt++
+    val currGame = gameCnt
+    val prevStates1 = mutableSetOf<Deck>()
+    val prevStates2 = mutableSetOf<Deck>()
+    var decks = startingDecks
+    var roundCnt = 0
+    while (decks.first.isNotEmpty() && decks.second.isNotEmpty()) {
+        roundCnt++
+        decks = recursiveRound(decks, prevStates1, prevStates2)
+        if (currGame == 1) {
+            println("-- Round $roundCnt (Game $currGame) --")
+            println("Player 1's deck: ${decks.first}")
+            println("Player 2's deck: ${decks.second}\n")
+        }
+    }
+    return decks
+}
+
+fun recursiveRound(decks: Pair<Deck, Deck>, prevRounds1: MutableSet<Deck>, prevRounds2: MutableSet<Deck>): Pair<Deck, Deck> {
+    val (p1, p2) = decks
+    if (prevRounds1.contains(p1) || prevRounds2.contains(p2)) {
+        return Pair(p1.drop(1) + p1.first() + p2.first(), p2.drop(1))
+    } else {
+        prevRounds1.add(p1)
+        prevRounds2.add(p2)
+    }
+    return if ((p1.size - 1) >= p1.first() && (p2.size - 1) >= p2.first()) {
+        val (sp1, sp2) = recursiveGame(Pair(
+                p1.drop(1).take(p1.first()),
+                p2.drop(1).take(p2.first())
+        ))
+        if (sp1.size > sp2.size) {
+            Pair(p1.drop(1) + p1.first() + p2.first(), p2.drop(1))
+        } else {
+            Pair(p1.drop(1), p2.drop(1) + p2.first() + p1.first())
+        }
+    } else {
+        round(decks)
+    }
+}
+
 fun main() {
 //    val player1Deck = testPlayer1.map { it.toInt() }
     val player1Deck = realPlayer1.map { it.toInt() }
 //    val player2Deck = testPlayer2.map { it.toInt() }
     val player2Deck = realPlayer2.map { it.toInt() }
 
-    var decks = Pair(player1Deck, player2Deck)
-    while (decks.first.isNotEmpty() && decks.second.isNotEmpty()) {
-        decks = round(decks)
-    }
-    val winner = if (decks.first.isEmpty()) decks.second else decks.first
+    val (p1, p2) = game(Pair(player1Deck, player2Deck))
+    val winner = if (p1.isEmpty()) p2 else p1
     println("Winner's deck: $winner")
     println("Part1: ${winner.reversed().mapIndexed { index, i -> (index + 1) * i }.sum()}")
-}
 
+    val (p12, p22) = recursiveGame(Pair(player1Deck, player2Deck))
+    val winner2 = if (p12.isEmpty()) p22 else p12
+    println("Winner2's deck: $winner2")
+    println("Part2: ${winner2.reversed().mapIndexed { index, i -> (index + 1) * i }.sum()}")
+}
+// 33534 too high
 val testPlayer1 = """
     9
     2
