c88c79d7a2984831f2367c358081f6181065c2b8
Jonas Heineson <heineson@gmail.com>
2020-12-24 11:46:25 +0100

    Day 24/2

--- src/main/kotlin/se/heinszn/aoc2020/day24/main.kt
+++ src/main/kotlin/se/heinszn/aoc2020/day24/main.kt
@@ -24,20 +24,52 @@ fun getTarget(start: HexCoord, path: List<String>): HexCoord {
     return pos
 }
 
-fun part1(data: List<List<String>>) {
+fun isBlack(value: Int?) = (value ?: 0) % 2 == 1
+
+fun part1(data: List<List<String>>): MutableMap<HexCoord, Int> {
     val flipped = mutableMapOf<HexCoord, Int>()
     data.forEach {
         val t = getTarget(HexCoord(0, 0), it)
         flipped[t] = flipped.getOrDefault(t, 0) + 1
     }
-    val black = flipped.values.filter { it % 2 == 1 }.count()
-    println("Part1: $black")
+    val black = flipped.values.filter { isBlack(it) }.count()
+    println("Part1: $black\n")
+    return flipped
+}
+
+fun turn(flipped: Map<HexCoord, Int>): Map<HexCoord, Int> {
+    val all = mutableMapOf<HexCoord, Int>()
+    flipped.forEach { entry ->
+        all[entry.key] = flipped[entry.key] ?: 0
+        val ns = entry.key.surroundingNeighbors()
+        ns.forEach { all[it] = flipped.getOrDefault(it, 0) }
+    }
+
+    val res = mutableMapOf<HexCoord, Int>()
+    all.forEach { hc ->
+        val blackNeighbors = hc.key.surroundingNeighbors().filter { isBlack(all[it]) }.count()
+        if (isBlack(hc.value) && (blackNeighbors == 0 || blackNeighbors > 2)) {
+            res[hc.key] = hc.value + 1
+        } else if (!isBlack(hc.value) && blackNeighbors == 2) {
+            res[hc.key] = hc.value + 1
+        } else res[hc.key] = hc.value
+    }
+    return res
+}
+
+fun part2(flipped: Map<HexCoord, Int>) {
+    var current = flipped
+    for (i in 1..100) {
+        current = turn(current)
+        println("Day $i: ${current.values.filter { isBlack(it) }.count()}")
+    }
 }
 
 fun main() {
-    // val data = testData1.map { parseLine(it) }
+    //val data = testData1.map { parseLine(it) }
     val data = readFile({ parseLine(it) })
-    part1(data)
+    val flipped = part1(data)
+    part2(flipped)
 }
 
 val testData1 = """
